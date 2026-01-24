1f2f3f05dbd04c5fc820ace119b74af732d070c9
karmakarmeghdip <karmakarmeghdip@gmail.com>
2026-01-24 18:29:46 +0530

    fix: fixed issue with config and preloaded all wallpapers

--- modules/hyprpaper.nix
+++ modules/hyprpaper.nix
@@ -6,14 +6,19 @@
     settings = {
       ipc = "on";
       splash = false;
-      splash_offset = 2.0;
 
       preload = [
+        "/home/mkarmakar/Pictures/Wallpaper/bsod.png"
         "/home/mkarmakar/Pictures/Wallpaper/flowering-rain.png"
+        "/home/mkarmakar/Pictures/Wallpaper/girl-stars.png"
+        "/home/mkarmakar/Pictures/Wallpaper/sakura-gate.jpg"
+        "/home/mkarmakar/Pictures/Wallpaper/wallhaven-vqoo1p.jpg"
+        "/home/mkarmakar/Pictures/Wallpaper/wanderer.jpg"
+        "/home/mkarmakar/Pictures/Wallpaper/waterfall.png"
       ];
 
       wallpaper = [
-        ",/home/mkarmakar/Pictures/Wallpaper/flowering-rain.png"
+        "HDMI-A-2,/home/mkarmakar/Pictures/Wallpaper/girl-stars.png"
       ];
     };
   };
