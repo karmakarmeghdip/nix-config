fd423b67c8ee1b5720e8d80ef05e29ccf0b8a54e
karmakarmeghdip <karmakarmeghdip@gmail.com>
2026-03-04 01:49:50 +0530

    feat: changed animations and blur settings

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -94,7 +94,7 @@ in
         rounding = 10;
         blur = {
           enabled = true;
-          size = 10;
+          size = 6;
           passes = 3;
         };
         shadow = {
@@ -103,6 +103,22 @@ in
       };
       animations = {
         enabled = true;
+        bezier = [
+          "wind, 0.05, 0.9, 0.1, 1.05"
+          "winIn, 0.1, 1.1, 0.1, 1.1"
+          "winOut, 0.3, -0.3, 0, 1"
+          "liner, 1, 1, 1, 1"
+        ];
+        animation = [
+          "windows, 1, 6, wind, slide"
+          "windowsIn, 1, 6, winIn, slide"
+          "windowsOut, 1, 5, winOut, slide"
+          "windowsMove, 1, 5, wind, slide"
+          "border, 1, 1, liner"
+          "borderangle, 1, 30, liner, loop"
+          "fade, 1, 10, default"
+          "workspaces, 1, 5, wind"
+        ];
       };
       input = {
         kb_layout = "us";
