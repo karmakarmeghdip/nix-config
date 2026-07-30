668ef32ffc3a5415fd9348e6e8752b7287e1025f
karmakarmeghdip <karmakarmeghdip@gmail.com>
2026-07-30 19:08:40 +0530

    feat: enabled gamescope

--- configuration.nix
+++ configuration.nix
@@ -137,6 +137,9 @@
 
   programs.gamemode.enable = true;
 
+  programs.gamescope.enable = true;
+  programs.gamescope.capSysNice = true;
+
   programs.neovim = {
     enable = true;
     viAlias = true;
